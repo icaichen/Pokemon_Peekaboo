0530cd83507e1da8017b6c03b85c7996b778778d
icaichen <31282986+icaichen@users.noreply.github.com>
2021-09-17 04:00:07 +0800

    count remaining pairs

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@
 </template>
 
 <script>
-  import {ref, watch} from 'vue';
+  import {computed, ref, watch} from 'vue';
   import Card from './components/Card'
 
   export default {
@@ -27,8 +27,22 @@
     setup() {
       const cardList = ref([])
       const userSelection = ref([])
-      const status = ref('')
-      
+
+      const status = computed(() => {
+        if (remainingPairs.value === 0) {
+          return 'Player wins'
+        } else {
+          return `Remaining Pairs: ${remainingPairs.value}`
+        }
+      })
+
+      const remainingPairs = computed(()=> {
+        const remainingCards = cardList.value.filter(
+          card => card.matched === false).length
+        
+        return remainingCards / 2
+      })
+
       for (let i = 0; i < 16; i++) {
         cardList.value.push({
             value: i,
@@ -59,6 +73,7 @@
           cardList.value[cardTwo.position].matched = true
         } else {
           status.value = 'Mismatch!'
+
           cardList.value[cardOne.position].visible = false
           cardList.value[cardTwo.position].visible = false
 
