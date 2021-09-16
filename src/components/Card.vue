5742104f8486f6f5688942b62a4b56f38c7863e0
icaichen <31282986+icaichen@users.noreply.github.com>
2021-09-17 03:48:44 +0800

    fix one issue

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -34,7 +34,9 @@
 
 <template>
     <div class="card" @click="selectCard" >
-        <div v-if="visible" class="card-face is-front">{{value}}-{{matched}}</div>
+        <div v-if="visible" class="card-face is-front">
+            {{ value }}-{{ matched }}
+        </div>
         <div v-else class="card-face is-back">Back</div>
     </div>
 </template>
